6ed65d0e91269285c95bad7c054a772cb08205ba
HailStorm32 <demetriv.s.7@gmail.com>
2015-06-21 18:06:08 -0700

    Deleted Gyro.h and added code
    ---Deleted the "Gyro.h"
    >Was causing problems with the gyro code as a function
    
    +++Added the code from the "Gyro.h" into the Main program
    
    ---Deleted the old code from the loop in "QuadMain.ino"

--- QuadMain/QuadMain.ino
+++ QuadMain/QuadMain.ino
@@ -40,7 +40,7 @@ int Chan4 = 11; //Channel: 4 -- Rudder
 int arm = 1000; // pulse width in microseconds for arming
 int speedvalue = 1000; // pulse width in microseconds for operation  // TESTED (5/19/15) Equal to 1000 is idol || Greater than (possibly lower) 1150 is motor movement
 int currentSpeed = 1090;
-
+float Yaw = 0;
 
 #include "Arduino.h"
 
@@ -169,25 +169,111 @@ with a 20 ms delay between pulses. */
 
 }
 
-#include "Gyro.h"
-
-  int Skip = 0;
 
 void loop()
   {
 
- 
-  YawSet();
+      // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+    }
 
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
 
-    if(ypr[0] > 124)
-    {
-      digitalWrite(12, LOW);
-      digitalWrite(13, HIGH);
-     // delay(500);
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+           
+            Yaw = ypr[0] * 180/M_PI;
+           
+            
+            digitalWrite(13, HIGH);
+            
+            if(Yaw > 123)
+            {
+              
+            digitalWrite(12, HIGH);
+              
+              
+            }
+            
+            else
+            {
+              
+            digitalWrite(12, LOW);
+              
+            }
+            
+            
+            
+            
+            
     }
     
-      digitalWrite(13, LOW);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
   
    
