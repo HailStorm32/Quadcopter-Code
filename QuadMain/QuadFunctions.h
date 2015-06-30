edad976889824f72bb254df70937952c75abc655
HailStorm32 <demetriv.s.7@gmail.com>
2015-06-30 16:00:20 -0700

    Time test
    +++Moved gyro code into motor function
    
    ---made gyro variables into int

--- QuadMain/QuadFunctions.h
+++ QuadMain/QuadFunctions.h
@@ -1,8 +1,10 @@
 int time = 0;
 
-//VVVVVVV---------------------------SingleChan------------------------------------------------------------------------------VVVVV
-void SingleChan(int escPin, int speed, int millisec)
+//VVVVVVV---------------------------BoardArm------------------------------------------------------------------------------VVVVV
+void BoardArm(int chanVal1, int chanVal2)
 {
+  int millisec = 1500;
+  
   int stoptime = 0;
 
   time = millis();
@@ -11,9 +13,44 @@ void SingleChan(int escPin, int speed, int millisec)
   
   while(true) //Needs to be in loop to provide complete wave forms
   {
-    digitalWrite(escPin, HIGH);
+    digitalWrite(chanVal1, HIGH);
+    delayMicroseconds(1086);
+    digitalWrite(chanVal1, LOW);
+    
+    digitalWrite(chanVal2, HIGH);
+    delayMicroseconds(1875);
+    digitalWrite(chanVal2, LOW);        
+   
+    time = millis();
+   
+    if(time > stoptime)
+    {
+      break;
+    }
+    
+  } 
+}
+//^^^^^^^---------------------------BoardArm------------------------------------------------------------------------------^^^^^
+
+
+//VVVVVVV---------------------------SingleChan------------------------------------------------------------------------------VVVVV
+void SingleChan(int chanVal, int speed, int millisec)
+{
+  
+  if(millisec > 0)
+  {
+  
+  int stoptime = 0;
+
+  time = millis();
+
+  stoptime = time + millisec;
+  
+  while(true) //Needs to be in loop to provide complete wave forms
+  {
+    digitalWrite(chanVal, HIGH);
     delayMicroseconds(speed);
-    digitalWrite(escPin, LOW);
+    digitalWrite(chanVal, LOW);
     delay(20);
    
     time = millis();
@@ -23,6 +60,216 @@ void SingleChan(int escPin, int speed, int millisec)
       break;
     }
   }
+  }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////  
+  else if(millisec == 0)
+  {
+   
+    int Stop;
+    
+  while(true) //Needs to be in loop to provide complete wave forms
+  {
+     
+    line73:    
+    
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+    }
+
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);           
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print(", ");             
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print(", ");                         
+            Serial.println(ypr[2] * 180/M_PI);
+
+            
+            Yaw = ypr[0] * 180/M_PI;
+            Pitch = ypr[1] * 180/M_PI;
+            Roll = ypr[2] * 180/M_PI;
+
+////////VVVVVVVV//////////////RUNS ONCE/////////////VVVVVVVV//////////////////////          
+         
+         if(Skip == 10)
+         {
+            YavgMx = Yaw + 10;
+            YavgMi = Yaw - 10;
+            PavgMx = Pitch + 10;
+            PavgMi = Pitch - 10;
+            RavgMx = Roll + 10;             
+            RavgMi = Roll - 10;             
+
+            Serial.print(YavgMx);
+            Serial.print(", ");             
+            Serial.print(YavgMi);
+            Serial.print(", ");                         
+            Serial.println(PavgMx);            
+            Serial.print(", ");                         
+            Serial.print(PavgMi);
+            Serial.print(", ");             
+            Serial.print(RavgMx);
+            Serial.print(", ");                         
+            Serial.println(RavgMi);            
+
+            digitalWrite(4, LOW);
+            digitalWrite(13, HIGH);          
+
+            Skip = 5;
+         }
+         
+         if(Skip == 0)
+          { 
+            int millisec = 20000;
+           
+            int stoptime = 0;
+
+            time = millis();
+
+            stoptime = time + millisec;
+            Serial.println("Calibrating Gyro....");                         
+  
+            while(true) 
+            {   
+               digitalWrite(4, HIGH);
+               
+               time = millis();
+               
+               if(time > stoptime)
+               {
+                BoardArm(Chan3, Chan4);
+                Skip = 10;             
+                goto line73;
+                //break;
+               }
+             }
+          }
+          
+///////////^^^^^^^////////////RUNS ONCE//////////////^^^^^^^^//////////////////   
+ 
+    digitalWrite(chanVal, HIGH);
+    delayMicroseconds(speed);
+    digitalWrite(chanVal, LOW);
+   // delay(300);   
+   
+ if(Yaw > YavgMx)
+            {
+              
+            SingleChan(Chan4, 1665, 2);
+              
+            digitalWrite(8, LOW);   
+            digitalWrite(12, HIGH); 
+              
+              
+            }
+            
+            else if(Yaw < YavgMi)
+            {
+              
+            SingleChan(Chan4, 1265, 2);
+              
+            digitalWrite(8, HIGH);   
+            digitalWrite(12, LOW); 
+              
+            }
+            
+            else
+            {             
+              
+            digitalWrite(8, LOW);   
+            digitalWrite(12, LOW); 
+
+              
+            }
+//////////////////////////////////////////////////////////////////////            
+//            if(Pitch > PavgMx)
+//            {
+//              
+//             //CODE HERE              
+//              
+//            }
+//            
+//            else if(Pitch < PavgMi)
+//            {
+//              
+//            digitalWrite(8, LOW);   
+//            digitalWrite(12, LOW); 
+//              
+//            }
+//
+//            else
+//            {
+//             //CODE HERE              
+//            }            
+/////////////////////////////////////////////////////////////////////            
+//            if(Roll > RavgMx)
+//            {
+//              
+//            digitalWrite(8, LOW);   
+//            digitalWrite(12, LOW); 
+//              
+//            }
+//            
+//            else if(Roll < RavgMi)
+//            {
+//              
+//             //CODE HERE              
+//              
+//            }
+//            
+//            else
+//            {
+//             //CODE HERE              
+//            }
+//   
+//    if(Stop > 0)
+//    {
+//      break;
+//    }
+  }    
+  }
+  } 
+  
 }
 //^^^^^^^---------------------------SingleChan------------------------------------------------------------------------------^^^^^
 
@@ -64,41 +311,6 @@ void FourChan(int chanVal1, int chanVal2, int chanVal3, int chanVal4, int speed1
   }
 }
 //^^^^^^^---------------------------FourChan------------------------------------------------------------------------------^^^^^
-
-
-
-//VVVVVVV---------------------------BoardArm------------------------------------------------------------------------------VVVVV
-void BoardArm(int chanVal1, int chanVal2)
-{
-  int millisec = 1500;
-  
-  int stoptime = 0;
-
-  time = millis();
-
-  stoptime = time + millisec;
-  
-  while(true) //Needs to be in loop to provide complete wave forms
-  {
-    digitalWrite(chanVal1, HIGH);
-    delayMicroseconds(1086);
-    digitalWrite(chanVal1, LOW);
-    
-    digitalWrite(chanVal2, HIGH);
-    delayMicroseconds(1875);
-    digitalWrite(chanVal2, LOW);        
-   
-    time = millis();
-   
-    if(time > stoptime)
-    {
-      break;
-    }
-    
-  } 
-}
-//^^^^^^^---------------------------BoardArm------------------------------------------------------------------------------^^^^^
-
 
 
 //VVVVVVV---------------------------unArm------------------------------------------------------------------------------VVVVV
@@ -205,7 +417,21 @@ void ThrSet(int Chan1, int Setspeed, int millisec)
 
 
 }
+//^^^^^^^---------------------------ThrSet------------------------------------------------------------------------------^^^^^
 
+//VVVVVVV---------------------------Route------------------------------------------------------------------------------VVVVV
+void Route()
+{
+  
+  
+  
+   SingleChan(Chan3, 1150, 2);
+  
+  
+  
+  
+}
+//^^^^^^^---------------------------Route------------------------------------------------------------------------------^^^^^
 
 
 
--- QuadMain/QuadMain.ino
+++ QuadMain/QuadMain.ino
@@ -40,13 +40,16 @@ int Chan4 = 11; //Channel: 4 -- Rudder
 int arm = 1000; // pulse width in microseconds for arming
 int speedvalue = 1000; // pulse width in microseconds for operation  // TESTED (5/19/15) Equal to 1000 is idol || Greater than (possibly lower) 1150 is motor movement
 int currentSpeed = 1090;
-float Yaw = 0;
-float Pitch = 0;
-float Roll = 0;
+int Yaw = 0;
+int Pitch = 0;
+int Roll = 0;
 int Skip = 0;
-float Yavg = 0;
-float Pavg = 0;
-float Ravg = 0;
+int YavgMx = 0;
+int YavgMi = 0;
+int PavgMx = 0;
+int PavgMi = 0;
+int RavgMx = 0;             
+int RavgMi = 0; 
 
 
 
@@ -183,169 +186,123 @@ with a 20 ms delay between pulses. */
 void loop()
   {
 
-      // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
-        // other program behavior stuff here
-        // .
-        // .
-        // .
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
-    }
-
-    // reset interrupt flag and get INT_STATUS byte
-    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
-
-    // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-        // reset so we can continue cleanly
-        mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpuIntStatus & 0x02) {
-        // wait for correct available data length, should be a VERY short wait
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-        // read a packet from FIFO
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
-        fifoCount -= packetSize;
-
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);           
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print(", ");             
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print(", ");                         
-            Serial.println(ypr[2] * 180/M_PI);
-
+//      // wait for MPU interrupt or extra packet(s) available
+//    while (!mpuInterrupt && fifoCount < packetSize) {
+//        // other program behavior stuff here
+//        // .
+//        // .
+//        // .
+//        // if you are really paranoid you can frequently test in between other
+//        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+//        // while() loop to immediately process the MPU data
+//        // .
+//        // .
+//        // .
+//    }
+//
+//    // reset interrupt flag and get INT_STATUS byte
+//    mpuInterrupt = false;
+//    mpuIntStatus = mpu.getIntStatus();
+//
+//    // get current FIFO count
+//    fifoCount = mpu.getFIFOCount();
+//
+//    // check for overflow (this should never happen unless our code is too inefficient)
+//    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+//        // reset so we can continue cleanly
+//        mpu.resetFIFO();
+//        Serial.println(F("FIFO overflow!"));
+//
+//    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+//    } else if (mpuIntStatus & 0x02) {
+//        // wait for correct available data length, should be a VERY short wait
+//        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+//
+//        // read a packet from FIFO
+//        mpu.getFIFOBytes(fifoBuffer, packetSize);
+//        
+//        // track FIFO count here in case there is > 1 packet available
+//        // (this lets us immediately read more without waiting for an interrupt)
+//        fifoCount -= packetSize;
+//
+//            // display Euler angles in degrees
+//            mpu.dmpGetQuaternion(&q, fifoBuffer);
+//            mpu.dmpGetGravity(&gravity, &q);
+//            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+//
+//            mpu.dmpGetQuaternion(&q, fifoBuffer);
+//            mpu.dmpGetGravity(&gravity, &q);
+//            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);           
+//            Serial.print(ypr[0] * 180/M_PI);
+//            Serial.print(", ");             
+//            Serial.print(ypr[1] * 180/M_PI);
+//            Serial.print(", ");                         
+//            Serial.println(ypr[2] * 180/M_PI);
+//
+//            
+//            Yaw = ypr[0] * 180/M_PI;
+//            Pitch = ypr[1] * 180/M_PI;
+//            Roll = ypr[2] * 180/M_PI;
+//
+//////////VVVVVVVV//////////////RUNS ONCE/////////////VVVVVVVV//////////////////////          
+//         if(Skip == 10)
+//         {
+//            YavgMx = Yaw + 10;
+//            YavgMi = Yaw - 10;
+//            PavgMx = Pitch + 10;
+//            PavgMi = Pitch - 10;
+//            RavgMx = Roll + 10;             
+//            RavgMi = Roll - 10;             
+//
+//            Serial.print(YavgMx);
+//            Serial.print(", ");             
+//            Serial.print(YavgMi);
+//            Serial.print(", ");                         
+//            Serial.println(PavgMx);            
+//            Serial.print(", ");                         
+//            Serial.print(PavgMi);
+//            Serial.print(", ");             
+//            Serial.print(RavgMx);
+//            Serial.print(", ");                         
+//            Serial.println(RavgMi);            
+//
+//            digitalWrite(4, LOW);
+//            digitalWrite(13, HIGH);          
+//
+//            Skip = 5;
+//         }
+//         
+//         if(Skip == 0)
+//          { 
+//            int millisec = 20000;
+//           
+//            int stoptime = 0;
+//
+//            time = millis();
+//
+//            stoptime = time + millisec;
+//            Serial.println("Calibrating Gyro....");                         
+//  
+//            while(true) 
+//            {   
+//               digitalWrite(4, HIGH);
+//               
+//               time = millis();
+//               
+//               if(time > stoptime)
+//               {
+//                BoardArm(Chan3, Chan4);
+//                Skip = 10;             
+//                break;
+//               }
+//             }
+//          }
+/////////////^^^^^^^////////////RUNS ONCE//////////////^^^^^^^^//////////////////                     
+          
+          
+            SingleChan(Chan3, 1150, 0);                         
             
-            Yaw = ypr[0] * 180/M_PI;
-            Pitch = ypr[1] * 180/M_PI;
-            Roll = ypr[2] * 180/M_PI;
- 
-         if(Skip == 10)
-         {
-            Yavg = Yaw;
-            Pavg = Pitch;
-            Ravg = Roll;             
-
-            Serial.print(Yavg);
-            Serial.print(", ");             
-            Serial.print(Pavg);
-            Serial.print(", ");                         
-            Serial.println(Ravg);            
-
-            digitalWrite(4, LOW);
-            digitalWrite(13, HIGH);          
-
-            Skip = 5;
-         }
-         
-         
-         if(Skip == 0)
-          { 
-            int millisec = 20000;
            
-            int stoptime = 0;
-
-            time = millis();
-
-            stoptime = time + millisec;
-            Serial.println("Calibrating Gyro....");                         
-  
-            while(true) 
-            {   
-               digitalWrite(4, HIGH);
-               
-               time = millis();
-               
-               if(time > stoptime)
-               {
-                Skip = 10;             
-                break;
-               }
-             }
-          }
-            
-                                    
-            if(Yaw > Yavg)
-            {
-              
-            digitalWrite(8, LOW);   
-            digitalWrite(12, HIGH); 
-              
-              
-            }
-            
-            else if(Yaw < Yavg)
-            {
-              
-            digitalWrite(8, HIGH);   
-            digitalWrite(12, LOW); 
-              
-            }
-            
-            else
-            {
-            digitalWrite(8, LOW);   
-            digitalWrite(12, LOW); 
-            }
-////////////////////////////////////////////////////////////////////            
-            if(Pitch > Pavg)
-            {
-              
-             //CODE HERE              
-              
-            }
-            
-            else if(Pitch < Pavg)
-            {
-              
-             //CODE HERE              
-              
-            }
-
-            else
-            {
-             //CODE HERE              
-            }            
-///////////////////////////////////////////////////////////////////            
-            if(Roll > Ravg)
-            {
-              
-             //CODE HERE              
-              
-            }
-            
-            else if(Roll < Ravg)
-            {
-              
-             //CODE HERE              
-              
-            }
-            
-            else
-            {
-             //CODE HERE              
-            }
             
             
     }
@@ -383,5 +340,5 @@ void loop()
   
    
 
-  }
+
 
